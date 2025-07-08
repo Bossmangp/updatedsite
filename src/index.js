6b138c329eafd36805554fb8982c02dceee505cf
Bossmangp <Mrmonk1011@gmail.com>
2025-07-08 16:40:55 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import DJGeeWebsite from './App';
 import './index.css';
+import DJGeeWebsite from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <DJGeeWebsite />
-  </React.StrictMode>
-);
+root.render(<DJGeeWebsite />);
